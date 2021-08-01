fbd62dbd63f5049f87e30b606cfcab58975dbfeb
aliazhar-id <aliazhar0555@gmail.com>
2021-08-01 08:57:33 +0700

    Add This on arrow function

---
+++ 03. JavaScript-Advanced/02. thisarrowfunction/script.js
@@ -0,0 +1,14 @@
+const box = document.querySelector('.box');
+box.addEventListener('click', function() {
+  let size = 'size';
+  let caption = 'caption'
+
+  if (this.classList.contains(size)) {
+    [size, caption] = [caption, size];
+  }
+
+  this.classList.toggle(size);
+  setTimeout( () => {
+    this.classList.toggle(caption);
+  }, 600);
+})
